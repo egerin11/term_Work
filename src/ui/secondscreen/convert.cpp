5f4556ba2b0fac3ed323aee725ead01ad5d08e5e
egerin <zhenya.eww@gmail.com>
2023-12-06 01:10:25 +0300

    program version 0.0.1

--- src/ui/secondscreen/convert.cpp
+++ src/ui/secondscreen/convert.cpp
@@ -8,8 +8,7 @@
 #include <cmath>
 
 
-
-void Convert::ascii(const Image *image,const std::string& file_path) {
+void Convert::ascii(const Image *image, const std::string &file_path) {
     const std::string ASCII_LIST = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
     std::vector<std::vector<Pixel>> pixels = image->get_pixels();
 
@@ -18,7 +17,8 @@ void Convert::ascii(const Image *image,const std::string& file_path) {
     if (output_file.is_open()) {
         for (int y = 0; y < image->get_height(); y++) {
             for (int x = 0; x < image->get_width(); x++) {
-                int ascii_index = static_cast<int>(map(static_cast<float>(pixels[y][x].m_red), 0, 255, 0, static_cast<float>(ASCII_LIST.length()) - 1));
+                int ascii_index = static_cast<int>(map(static_cast<float>(pixels[y][x].m_red), 0, 255, 0,
+                                                       static_cast<float>(ASCII_LIST.length()) - 1));
                 char ascii_char = ASCII_LIST[ascii_index];
 
                 output_file << ascii_char;
@@ -31,10 +31,11 @@ void Convert::ascii(const Image *image,const std::string& file_path) {
         std::cerr << "Unable to open the output file." << std::endl;
     }
 }
-    float Convert::map(float value, float start1, float stop1, float start2, float stop2) {
-        return ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
 
-    }
+float Convert::map(float value, float start1, float stop1, float start2, float stop2) {
+    return ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+
+}
 
 void Convert::grayscale(Image *image) {
     std::vector<std::vector<Pixel>> pixels = image->get_pixels();
@@ -54,11 +55,9 @@ void Convert::grayscale(Image *image) {
 }
 
 
-
-
 std::vector<std::vector<Pixel>> Convert::resized_image(Image *image, int new_height, int new_width) {
-    std::vector<std::vector<Pixel>> resized_image(new_height, std::vector<Pixel>(new_width));
     std::vector<std::vector<Pixel>> pixel = image->get_pixels();
+    std::vector<std::vector<Pixel>> resized_image(new_height, std::vector<Pixel>(new_width));
 
     int old_h = image->get_height();
     int old_w = image->get_width();
@@ -83,7 +82,7 @@ std::vector<std::vector<Pixel>> Convert::resized_image(Image *image, int new_hei
                 Pixel q1 = pixel[x_floor][y_ceil];
                 Pixel q2 = pixel[x_floor][y_floor];
                 Pixel q;
-                q = q1* (y_ceil - y) + q2 * (y - y_floor);
+                q = q1 * (y_ceil - y) + q2 * (y - y_floor);
 
                 resized_image[i][j] = q;
             } else if (y_ceil == y_floor) {
@@ -99,7 +98,7 @@ std::vector<std::vector<Pixel>> Convert::resized_image(Image *image, int new_hei
                 Pixel v3 = pixel[x_floor][y_ceil];
                 Pixel v4 = pixel[x_ceil][y_ceil];
                 Pixel q1, q2, q;
-                q1 = v1 * (x_ceil - x) + v2* (x - x_floor);
+                q1 = v1 * (x_ceil - x) + v2 * (x - x_floor);
 
                 q2 = v3 * (x_ceil - x) + v4 * (x - x_floor);
 
